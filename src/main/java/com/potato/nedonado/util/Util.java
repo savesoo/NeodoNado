47bede9ff3d557d1755258453f5c5cf535392df6
Seo-Aram <arams.office@gmail.com>
2022-12-02 10:12:51 +0900

    timeconvert 추가

--- src/main/java/com/potato/nedonado/util/Util.java
+++ src/main/java/com/potato/nedonado/util/Util.java
@@ -1,6 +1,12 @@
 package com.potato.nedonado.util;
 
 import javax.servlet.http.Cookie;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Util {
 
@@ -21,5 +27,11 @@ public class Util {
 
     public static long getCurrentTimestamp() {
         return System.currentTimeMillis();
+    }
+
+    public static String convertLongTimestampToString(long timestamp) {
+        LocalDateTime localDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp/1000),
+                TimeZone.getDefault().toZoneId());
+        return DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss").format(localDate);
     }
 }
