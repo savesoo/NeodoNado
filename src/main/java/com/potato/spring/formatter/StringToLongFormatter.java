182a9d9ce85af3092904db56517b5d3f27d76b6b
Seo-Aram <arams.office@gmail.com>
2022-12-02 09:53:10 +0900

    Update StringToLongFormatter.java

---
+++ src/main/java/com/potato/spring/formatter/StringToLongFormatter.java
@@ -0,0 +1,18 @@
+package com.potato.spring.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+public class StringToLongFormatter implements Formatter<Long> {
+    @Override
+    public Long parse(String text, Locale locale) throws ParseException {
+        return Long.parseLong(text);
+    }
+
+    @Override
+    public String print(Long object, Locale locale) {
+        return Long.parseLong(text);
+    }
+}
