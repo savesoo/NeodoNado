8b1fa68d981df37305382de0234d781194865572
Seo-Aram <arams.office@gmail.com>
2022-12-03 10:47:10 +0900

    redirect후 return 처리

--- src/main/java/com/potato/spring/Filter/LoginCheckFilter.java
+++ src/main/java/com/potato/spring/Filter/LoginCheckFilter.java
@@ -37,6 +37,7 @@ public class LoginCheckFilter implements Filter {
 
             log.info(" >>> 비로그인 상태 -> 로그인 페이지로 이동 >>> ");
             res.sendRedirect("/user/login");
+            return;
 
         }
 
