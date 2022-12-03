2f1dd42cba49f38007da91b80c07b42c958e606e
savesoo <hsw2657@gmail.com>
2022-12-03 14:51:52 +0900

    service단 수정
    .

--- src/main/java/com/potato/nedonado/service/comment/CommentWriteService.java
+++ src/main/java/com/potato/nedonado/service/comment/CommentWriteService.java
@@ -1,14 +1,25 @@
 package com.potato.nedonado.service.comment;
 
 import com.potato.nedonado.mapper.CommentMapper;
+import com.potato.nedonado.model.board.ItemEntity;
+import com.potato.nedonado.model.comment.CommentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentWriteService {
 
     @Autowired(required = false)
     private CommentMapper commentMapper;
+
+    public List<CommentDTO> selectAllComments(int boardIdx){
+
+        ItemEntity item = ItemEntity.builder().boardIdx(boardIdx).build();
+
+        return commentMapper.selectAllComments(boardIdx);
+    }
 
 
 }
